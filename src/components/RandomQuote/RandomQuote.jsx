0955cef82d31e1daeaeae855771aafd7ea2e91e9
dylstar135 <dylstar160990@googlemail.com>
2024-01-17 09:46:18 +0000

    added background image, and a fade effect on each new quote loading. Struggled to  find a way to get each quote to fade, but over came it eventually with a key effect

--- src/components/RandomQuote/RandomQuote.jsx
+++ src/components/RandomQuote/RandomQuote.jsx
@@ -1,49 +1,65 @@
-import React, { useState } from 'react'
-import './RandomQuote.css'
-import twitter_icon from '../assets/twitter.png'
-import reload_icon from '../assets/reload.png'
+import React, { useState, useEffect, useCallback, useRef } from 'react';
+import './RandomQuote.css';
+import twitter_icon from '../assets/twitter.png';
+import reload_icon from '../assets/reload.png';
+
 const RandomQuote = () => {
+    // Ref to store quotes for random selection
+    const quotesRef = useRef([]);
 
-    let quotes = [];
-
-    async function loadQuotes(){
-        const response = await fetch('https://type.fit/api/quotes ');
-        quotes = await response.json();
-    }
-
-
-    const [quote,setQuote] = useState({
+    // State to store the current quote
+    const [quote, setQuote] = useState({
         text: 'Not all those who wander are lost',
         author: 'JRR Tolkein',
     });
 
-    const random = ()=>{
-        const select = quotes[Math.floor(Math.random()*quotes.length)]
+    // Function to fetch quotes and update the ref
+    const loadQuotes = useCallback(async () => {
+        const response = await fetch('https://type.fit/api/quotes');
+        quotesRef.current = await response.json();
+    }, []);
+
+    // Function to generate a random quote
+    const random = () => {
+        // Select a random quote from the ref
+        const select = quotesRef.current[Math.floor(Math.random() * quotesRef.current.length)];
         setQuote(select);
     };
 
-    const twitter = ()=>{
-window.open(`https://twitter.com/intent/tweet?text=${quote.text}-${quote.author.split(',')[0]}`);
+    // Function to share quote on Twitter
+    const twitter = () => {
+        window.open(`https://twitter.com/intent/tweet?text=${quote.text}-${quote.author.split(',')[0]}`);
     };
 
-    loadQuotes();
+    // Load quotes on component mount
+    useEffect(() => {
+        loadQuotes();
+    }, [loadQuotes]);
 
     return (
-    <div className = 'container'>
-        <div className="quote">{quote.text}</div>
-        <div>
-            <div className="line"></div>
-            <div className="bottom">
-                <div className="author">- {quote.author.split(',')[0]}</div>
-                <div className="icons">
-                    <img src={reload_icon}  onClick={()=>{random()}} alt="" />
-                    <img src={twitter_icon} onClick={()=> {twitter()}} alt="" />
+        // Use the quote.text as the key to trigger remount for fade-in effect
+        <div key={quote.text} className="container fade-in">
+            <div className="quote">{quote.text}</div>
+            <div>
+                <div className="line"></div>
+                <div className="bottom">
+                    <div className="author">- {quote.author.split(',')[0]}</div>
+                    <div className="icons">
+                        {/* Button to generate a new random quote */}
+                        <img src={reload_icon} onClick={random} alt="Reload Quote" />
+
+                        {/* Button to share the current quote on Twitter */}
+                        <img src={twitter_icon} onClick={twitter} alt="Share on Twitter" />
+                    </div>
                 </div>
             </div>
         </div>
+    );
+};
 
-    </div>
-  )
-}
+export default RandomQuote;
 
-export default RandomQuote
+
+
+
+
